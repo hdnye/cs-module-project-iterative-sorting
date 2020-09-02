5228e87837407ea3b76dd5a38b8064b5b57f89d5
Heather Nye <heatherdnye@gmail.com>
2020-09-02 18:55:06 -0500

    need swap code for iterative

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -8,6 +8,14 @@ def selection_sort(arr):
         # (hint, can do in 3 loc)
         # Your code here
 
+        # for loop for i after cur_index
+        for j in range(cur_index + 1, len(arr)):
+            # check each i against smallest i 
+            if arr[j] < arr[smallest_index]:
+                # replace smallest w j
+                smallest_index = j
+            # incrememnt outer loop & swap 
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
 
         # TO-DO: swap
         # Your code here
@@ -18,9 +26,17 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
-
+    n = len(arr)
+    # compare the first pair of elements
+    for i in range(n - 1):
+        for j in range(n - i -1):
+    #   if rhs is < lhs, swap
+    # iterate through list & compare adj pairs
+    # if 1 or more swaps, repeat
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]          
     return arr
+
 
 '''
 STRETCH: implement the Counting Sort function below
@@ -44,3 +60,32 @@ def counting_sort(arr, maximum=None):
 
 
     return arr
+
+def insertion_sort(arr):
+    # if part of the arr is sorted
+        # we can have an index to the first element that's unsorted
+        first_unsorted_index = 1
+        while first_unsorted_index < len(arr):
+        # take the next unsorted element
+            cur_element = arr[first_unsorted_index]
+        # compare to each of the elements in the sorted arr going from biggest to smallest
+            sorted_index = first_unsorted_index - 1
+            while  sorted_index >= 0:
+            # if sorted element > cur element, shift it 
+                if arr[sorted_index] > cur_element: 
+                    arr[sorted_index + 1] = arr[sorted_index]
+                    # decrement sorted index
+                    sorted_index = sorted_index - 1                   
+                elif arr[sorted_index] < cur_element:
+                    # we found the right place for it
+                    arr[sorted_index + 1] = cur_element
+                    # break
+                # when we get to the end
+                if sorted_index == 0:
+                    arr[sorted_index] = cur_element
+                    # decreement sorted index to keep looping backwards
+                sorted_index = sorted_index - 1
+                    # insert the current element into the array
+                arr[sorted_index + 1] = cur_element
+
+
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,58 @@
 def linear_search(arr, target):
+    pass
     # Your code here
-
-
+    # Loop trhought array
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
+    # compare values
+    # if end of list reached, rtn i not found
+    # else i++ & repeat until i found
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
     # Your code here
+    # check for empty []
+    if arr is None: 
+        return False
+   
+    # start = starting index of the subset of the arr we're searching in - Inclusive
+    # end = end index of the subset - inclusive
+    start = 0
+    end = len(arr) - 1
 
+    # High level algorithm: 
+    while  start <= end: 
+    # Start at middle
+    # how do we get the midpoint?
+    #   for the whole arr, it's (len(ar) / 2 - 1
+    #   for a subset of the arry: (start + end) // 2    
+        mid_index = (start + end) // 2
+        mid_index = arr[mid_index]
 
+    # Compare it to target
+        if target == mid_index:    
+            return mid_index     
+
+    # if target is < middle value, go left
+        if target < mid_index:
+            end = mid_index - 1 # eliminate lhs
+
+    # if target is > middle value, go right
+        if target > mid_index:
+        # set start = mid_index +1
+            start = mid_index + 1   # elimnate rhs  
+
+    # Repeat. Halving the BST until you either find the target or not
+    # if not found:     
+        # else: 
+        #     return mid_index
+
+    # Check if not found
+    #   if subset has a 0 or - length
+    # how do we represent the subset that we're searching in? 
+    #   store start and end index in variables
     return -1  # not found
+
